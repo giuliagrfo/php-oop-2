45d3df2eda8e41b0417f7bc7ca25d1c45e51b8cd
giuliagrfo <giuliagrifo1@gmail.com>
2022-12-12 16:10:31 +0100

    initial commit - add class product

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class Product
+{
+    public $name;
+    public $price;
+    public $description;
+
+
+    public function __construct(String $name, String $price, String $description)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
